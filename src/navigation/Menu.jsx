a1cdd234a55aee44d2dd43aea8648866983adec3
AldieryGonzalez <aldieryrgon@gmail.com>
2023-10-08 14:10:59 -0500

    Fixed Centering

--- src/navigation/Menu.jsx
+++ src/navigation/Menu.jsx
@@ -5,37 +5,37 @@ import AddIcon from "@mui/icons-material/Add";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 
 const Menu = () => {
-  return (
-    <div className="main-layout">
-      <header className="header">
-        <nav>
-          <h1 className="mx-auto" id="appName" data-cy="appName">
-            <span className="vertical-text">Lost</span>
-            <span className="nu-logo">
-              <img src="/nu-logo.jpg" alt="N" />
-            </span>
-            <span className="vertical-text">Found</span>
-          </h1>
-        </nav>
-      </header>
-      <main className="main-part">
-        <div style={{ marginTop: "0px" }}>
-          <Outlet />
-        </div>
-      </main>
-      <footer>
-        <nav>
-          <NavLink to="/">Found</NavLink>
-          <Fab color="primary" aria-label="add">
-            <NavLink className="post-button" to="postpage">
-              <AddIcon />
-            </NavLink>
-          </Fab>
-          <NavLink to="lostpage">Lost</NavLink>
-        </nav>
-      </footer>
-    </div>
-  );
+	return (
+		<div className='main-layout'>
+			<header className='header'>
+				<nav className='w-100'>
+					<h1 className='mx-auto' id='appName' data-cy='appName'>
+						<span className='vertical-text'>Lost</span>
+						<span className='nu-logo'>
+							<img src='/nu-logo.jpg' alt='N' />
+						</span>
+						<span className='vertical-text'>Found</span>
+					</h1>
+				</nav>
+			</header>
+			<main className='main-part'>
+				<div style={{ marginTop: "0px" }}>
+					<Outlet />
+				</div>
+			</main>
+			<footer>
+				<nav>
+					<NavLink to='/'>Found</NavLink>
+					<Fab color='primary' aria-label='add'>
+						<NavLink className='post-button' to='postpage'>
+							<AddIcon />
+						</NavLink>
+					</Fab>
+					<NavLink to='lostpage'>Lost</NavLink>
+				</nav>
+			</footer>
+		</div>
+	);
 };
 
 export default Menu;
